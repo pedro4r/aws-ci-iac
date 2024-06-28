50b7274c71f5abfe6f5ff5c6dc3801dd483ff581
Pedro Requiao <alonsofts@gmail.com>
2024-06-28 19:39:21 -0400

    test: configure S3 bucket

--- main.tf
+++ main.tf
@@ -5,8 +5,35 @@ terraform {
       version = "5.49.0"
     }
   }
+
+  backend "s3" {
+    bucket = "aws-ci-iac"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "aws-ci-iac"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "aws-ci-iac"
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
